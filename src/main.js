2de31a6708500860a883c2e9bbb56bc2627af287
Robert Ende <robert.ende@web.de>
2018-06-03 02:53:06 +0200

    added Sentry.io vue integration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from "vue";
+import Raven from "raven-js";
+import RavenVue from "raven-js/plugins/vue";
+
 // import "./plugins/vuetify";
 import App from "./App.vue";
 // import "./registerServiceWorker";
+
+// please remove this when forking the project
+Raven
+  .config("https://b9ea0305d0364a6daf825925f36b4e0d@sentry.io/280614")
+  .addPlugin(RavenVue, Vue)
+  .install();
 
 Vue.config.productionTip = true;
 
