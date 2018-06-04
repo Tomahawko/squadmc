845cacb2fe40bed21ade469e3067d44884f8325c
Robert Ende <robert.ende@web.de>
2018-06-04 19:17:40 +0200

    Set Sentry.io error reporting to only in production mode

--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import RavenVue from "raven-js/plugins/vue";
 import App from "./App.vue";
 // import "./registerServiceWorker";
 
+// Sentry.io error reporting tool
+// only used in production mode
 // please remove this when forking the project
-Raven
-  .config("https://b9ea0305d0364a6daf825925f36b4e0d@sentry.io/280614")
-  .addPlugin(RavenVue, Vue)
-  .install();
+if (process.env.NODE_ENV === "production") {
+  Raven
+    .config("https://b9ea0305d0364a6daf825925f36b4e0d@sentry.io/280614")
+    .addPlugin(RavenVue, Vue)
+    .install();
+}
 
 Vue.config.productionTip = true;
 
